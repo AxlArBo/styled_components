b4d2977e78c1a1c008d668a36efb3f8275b50e95
Axel <axel.man94gmail.com>
2023-05-12 10:44:32 -0600

    Creando cards de los gastos que se han hecho con sus respectivos estilos

---
+++ src/ImageFilter.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import alimentacion from './assets/images/alimentacion.svg';
+import salud from './assets/images/salud.svg';
+import otros from './assets/images/otros.svg';
+import transporte from './assets/images/transporte.svg';
+import utilidades from './assets/images/utilidades.svg';
+import { Icon } from './Components/Variable';
+
+export default (type) => {
+    const Images = {
+        Restaurante: <Icon src={alimentacion} alt='Restaurante' />,
+        Salud: <Icon src={salud} alt='Salud' />,
+        Transporte: <Icon src={transporte} alt='Transporte' />,
+        Utilidades: <Icon src={utilidades} alt='Utilidades' />,
+        default: <Icon src={otros} alt='Otros' />,
+    }
+
+    return Images[type] || Images['default'];
+};
